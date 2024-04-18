77b853198f6636e2b598c449eda0782cc46aba3e
Ciubancan <cristian.ciubancan@clarisoft.com>
2024-04-18 19:27:43 +0300

    fixed

--- components/forms/input-field.tsx
+++ components/forms/input-field.tsx
@@ -5,7 +5,6 @@ import lodash from "lodash";
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
-  placeholder?: string;
   textarea?: boolean;
   handleChange?: any;
   validation?: RegisterOptions;
@@ -19,7 +18,6 @@ const InputField: React.FC<InputFieldProps> = ({
   validation,
   size: _,
   textarea,
-  placeholder,
   type,
 }) => {
   const {
@@ -44,7 +42,7 @@ const InputField: React.FC<InputFieldProps> = ({
             htmlFor={name}
             className={`${
               !!error ? "border-red-100" : ""
-            } min-h-10 absolute text-base text-white duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-yellow-500 peer-focus:dark:text-yellow-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto`}
+            } absolute text-base text-white duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-yellow-500 peer-focus:dark:text-yellow-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto`}
           >
             {label}
           </label>
@@ -62,7 +60,7 @@ const InputField: React.FC<InputFieldProps> = ({
             htmlFor={name}
             className={`${
               !!error ? "border-red-100" : ""
-            } bg-red-700 px-2 absolute min-h-10 text-base text-white duration-300 transform -translate-y-5 scale-75 top-3 z-10 origin-[0] start-2.5 peer-focus:text-yellow-500 peer-focus:dark:text-yellow-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto`}
+            } bg-red-700 px-2 absolute text-base text-white duration-300 transform -translate-y-5 scale-75 top-3 z-10 origin-[0] start-2.5 peer-focus:text-yellow-500 peer-focus:dark:text-yellow-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto`}
           >
             {label}
           </label>
