cb2cd46ae110980e8a19f4ec5e849dcede9f7a4c
Ciubancan <cristian.ciubancan@clarisoft.com>
2024-04-18 18:51:12 +0300

    update

--- components/forms/input-field.tsx
+++ components/forms/input-field.tsx
@@ -35,7 +35,7 @@ const InputField: React.FC<InputFieldProps> = ({
         <div className="relative">
           <input
             {...register(name, validation)}
-            className="block px-2.5 pb-2.5 pt-5 w-full text-xs text-white bg-red-700 border-0 border-b-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-yellow-500 peer"
+            className="block px-2.5 pb-2.5 pt-5 w-full min-h-10 text-xs text-white bg-red-700 border-0 border-b-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-yellow-500 peer"
             type={type ? type : "text"}
             id={name}
             placeholder=" "
@@ -44,7 +44,7 @@ const InputField: React.FC<InputFieldProps> = ({
             htmlFor={name}
             className={`${
               !!error ? "border-red-100" : ""
-            } absolute text-xs text-white duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-yellow-500 peer-focus:dark:text-yellow-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto`}
+            } min-h-10 absolute text-xs text-white duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-yellow-500 peer-focus:dark:text-yellow-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto`}
           >
             {label}
           </label>
@@ -55,14 +55,14 @@ const InputField: React.FC<InputFieldProps> = ({
             {...register(name, validation)}
             id={name}
             rows={4}
-            className="block p-2.5 w-full text-xs text-white bg-red-700 rounded-lg border-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-yellow-500 peer"
+            className="block p-2.5 w-full min-h-10 text-xs text-white bg-red-700 rounded-lg border-2 border-white appearance-none focus:outline-none focus:ring-0 focus:border-yellow-500 peer"
             placeholder=" "
           ></textarea>
           <label
             htmlFor={name}
             className={`${
               !!error ? "border-red-100" : ""
-            } bg-red-700 px-2 absolute text-xs text-white duration-300 transform -translate-y-5 scale-75 top-3 z-10 origin-[0] start-2.5 peer-focus:text-yellow-500 peer-focus:dark:text-yellow-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto`}
+            } bg-red-700 px-2 absolute min-h-10 text-xs text-white duration-300 transform -translate-y-5 scale-75 top-3 z-10 origin-[0] start-2.5 peer-focus:text-yellow-500 peer-focus:dark:text-yellow-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-5 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto`}
           >
             {label}
           </label>
