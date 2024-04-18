0dabec9ace2ab9d337abd2c698344aaed05f2370
Ciubancan <cristian.ciubancan@clarisoft.com>
2024-04-19 02:30:34 +0300

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import Header from "@/components/primitives/main-header";
 import "@/styles/global.css";
 import "@/styles/styles.css";
 import { Bebas_Neue } from "next/font/google";
-import Head from "next/head";
 
 const bebasNeue = Bebas_Neue({
   weight: ["400"],
@@ -17,9 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-red-700">
       {/* the following is a selective fix for IPhone's safari's zoom on input touch/focus */}
-      <Head>
-        <meta name="og:image" content="/images/og-image.jpg" />
-      </Head>
+      <meta property="og:image" content="/images/og-image.jpg" />
       <body className="position-relative">
         <Header />
         {children}
