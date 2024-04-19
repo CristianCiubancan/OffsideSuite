dee75e11ae434e66689678fd59775af0c66a4e79
Ciubancan <cristian.ciubancan@clarisoft.com>
2024-04-19 15:23:09 +0300

    done

--- components/primitives/main-header.tsx
+++ components/primitives/main-header.tsx
@@ -11,11 +11,11 @@ const Header = () => {
   return (
     <div className="w-full h-16 fixed top-0 left-0 z-10 overflow-hidden rounded-md">
       {/* a blurred navbar */}
-      <div className="blur-2xl h-16 w-full bg-white flex justify-between absolute top-0 left-0"></div>
+      <div className="blur-2xl h-16 w-full bg-red-700 flex justify-between absolute top-0 left-0"></div>
       <div
         className={`h-16 w-full flex justify-between items-center absolute z-20 top-0 left-0 p-4 text-white ${bebasNeue.className}`}
       >
-        <div className="font-bold text-2xl text-red-700">Offside Music</div>
+        <div className="font-bold text-2xl text-white">Offside Music</div>
         {/* TODO: maybe a add a menu once more functionalities are implemented */}
         {/* <Button
           color="yellow"
