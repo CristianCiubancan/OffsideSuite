c47399306398e3c7d8347ccf7c3dcc94ebcb35d5
Ciubancan <cristian.ciubancan@clarisoft.com>
2024-04-20 02:00:34 +0300

    fix la fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,32 +10,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="bg-red-700">
-      <Head>
-        {/* Include the title and meta description inside the Head */}
-        <title>Offside Music - Creative Media Agency</title>
-        <meta
-          name="description"
-          content="We are a creative media agency based in Bucharest specializing in music, video, lyrics, publicity, website and app development. Let's collaborate!"
-        />
+      {/* Include the title and meta description inside the Head */}
+      <title>Offside Music - Creative Media Agency</title>
+      <meta
+        name="description"
+        content="We are a creative media agency based in Bucharest specializing in music, video, lyrics, publicity, website and app development. Let's collaborate!"
+      />
 
-        {/* Open Graph and other meta tags */}
-        <meta
-          property="og:image"
-          content={`${process.env.NEXT_PUBLIC_FRONTEND_URL}/images/og-image.png`}
-        />
-        <meta property="og:url" content="https://www.offsidemusic.ro/" />
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:title"
-          content="Offside Music - Creative Media Agency"
-        />
-        <meta
-          property="og:description"
-          content="We are a creative media agency based in Bucharest specializing in music, video, lyrics, publicity, website and app development. Let's collaborate!"
-        />
-        <meta property="fb:app_id" content="450961180645048" />
-        <meta property="og:image:alt" content="OffsideMusic logo banner" />
-      </Head>
+      {/* Open Graph and other meta tags */}
+      <meta
+        property="og:image"
+        content={`${process.env.NEXT_PUBLIC_FRONTEND_URL}/images/og-image.png`}
+      />
+      <meta property="og:url" content="https://www.offsidemusic.ro/" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:title"
+        content="Offside Music - Creative Media Agency"
+      />
+      <meta
+        property="og:description"
+        content="We are a creative media agency based in Bucharest specializing in music, video, lyrics, publicity, website and app development. Let's collaborate!"
+      />
+      <meta property="fb:app_id" content="450961180645048" />
+      <meta property="og:image:alt" content="OffsideMusic logo banner" />
       <body className="position-relative">
         <Header />
         {children}
