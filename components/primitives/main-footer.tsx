ebd3742800566ea6ab081bcf31bc43e9a88211e0
Ciubancan <cristian.ciubancan@clarisoft.com>
2024-04-26 00:30:06 +0300

    fixed

--- components/primitives/main-footer.tsx
+++ components/primitives/main-footer.tsx
@@ -10,14 +10,7 @@ const Footer = () => {
   return (
     <nav className="w-full main-layout-footer bg-yellow-500 relative">
       <div className="container mx-auto p-8 relative z-10">
-        <div className="flex justify-between items-center flex-col">
-          <div className="pb-2 flex justify-center items-center flex-col w-full">
-            <p className="text-red-700 font-bold text-4xl text-center drop-shadow-lg">
-              <span className={`${bebasNeue.className} drop-shadow-md`}>
-                © 2024 Offside Music
-              </span>
-            </p>
-          </div>
+        <div className="flex justify-between items-center flex-col gap-4">
           <div className="text-xs display flex flex-wrap justify-center items-center">
             <Link
               href="/"
@@ -40,12 +33,21 @@ const Footer = () => {
               Privacy Policy
             </Link>
           </div>
-          <div className="text-black text-xs w-full text-center pt-4 drop-shadow-md">
-            envisioned, designed and developed by{" "}
+          <div className="flex justify-center items-center flex-col w-full">
+            <p className="text-red-700 font-bold text-4xl text-center drop-shadow-lg">
+              <span className={`${bebasNeue.className} drop-shadow-md`}>
+                © 2024 Offside Music
+              </span>
+            </p>
           </div>
-          <span className="text-black text-sm w-full text-center drop-shadow-lg">
-            Cristian Ciubăncan
-          </span>
+          <div className="flex justify-center items-center flex-col w-full">
+            <div className="text-black text-xs w-full text-center drop-shadow-md">
+              envisioned, designed and developed by{" "}
+            </div>
+            <span className="text-black text-sm w-full text-center drop-shadow-lg">
+              Cristian Ciubăncan
+            </span>
+          </div>
         </div>
       </div>
     </nav>
