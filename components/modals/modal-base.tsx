ede7fd99b454ab74347836fdf3c192a16eeace26
Ciubancan <cristian.ciubancan@clarisoft.com>
2024-04-26 15:49:53 +0300

    fixed

--- components/modals/modal-base.tsx
+++ components/modals/modal-base.tsx
@@ -10,10 +10,15 @@ interface IModalBase {
 const ModalBase = ({ children }: IModalBase) => {
   const modalRef = useRef<HTMLDivElement>(null);
   const { isOpen, closeModal, additionalData } = useModal();
-
+  const [isFinallyClosed, setIsFinallyClosed] = useState(false);
   useEffect(() => {
     if (isOpen) {
       document.body.style.overflow = "hidden";
+      setIsFinallyClosed(false);
+    } else {
+      setTimeout(() => {
+        setIsFinallyClosed(true);
+      }, 300);
     }
     return () => {
       document.body.style.overflow = "auto";
@@ -24,7 +29,7 @@ const ModalBase = ({ children }: IModalBase) => {
     <div
       // the animation duration must match modalAnimationDuration from modal-context.tsx
       className={`position-fixed top-0 left-0 w-full h-full ${
-        isOpen
+        !isFinallyClosed
           ? "opacity-100 pointer-events-auto"
           : "opacity-0 pointer-events-none"
       }`}
@@ -39,7 +44,7 @@ const ModalBase = ({ children }: IModalBase) => {
           e.stopPropagation();
           closeModal();
         }}
-        className={`fixed z-50 flex justify-center items-center inset-0 p-4 transition-opacity duration-300 ${
+        className={`fixed z-50 flex justify-center items-center inset-0 p-4 transition-all ease-in-out duration-300 ${
           isOpen
             ? "opacity-100 pointer-events-auto"
             : "opacity-0 pointer-events-none"
@@ -67,107 +72,8 @@ const ModalBase = ({ children }: IModalBase) => {
           </div>
         </div>
       </div>
-      {/* <div
-        onClick={closeModal}
-        className="bg-yellow-950 fixed inset-0 z-40 opacity-40"
-      ></div>
-      <div
-        onClick={(e) => {
-          e.stopPropagation();
-          closeModal();
-        }}
-        className="fixed z-50 flex justify-center items-center inset-0 p-4"
-      >
-        <div
-          style={{
-            height: additionalData?.forceButtonFix ? "100%" : "unset",
-          }}
-          className="relative max-w-screen-sm w-full"
-        >
-          <div
-            onClick={(e) => e.stopPropagation()}
-            className="overflow-auto p-8 rounded-lg border-0 bg-red-700 w-full h-full"
-          >
-            <div className="w-full flex items-center justify-center">
-              {children}
-            </div>
-          </div>
-          <div className="absolute top-0 right-0 mt-2 mr-2 z-50">
-            <Button onClick={closeModal} color="red">
-              <CancelIcon width="24" height="24" />
-            </Button>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
-
-  // return modalContent;
 };
 
 export default ModalBase;
-
-// "use client";
-// import React, { useEffect, useRef, useState } from "react";
-// import { useModal } from "@/components/contexts/modal-context";
-// import CancelIcon from "@/assets/icons/CancelIcon";
-// import Button from "@/components/primitives/button";
-// interface IModalBase {
-//   children: React.ReactNode;
-//   forceButtonFix?: boolean;
-// }
-// const ModalBase = ({ children, forceButtonFix }: IModalBase) => {
-//   const modalRef = useRef<HTMLDivElement>(null);
-//   const { isOpen, closeModal } = useModal();
-
-//   useEffect(() => {
-//     if (isOpen) {
-//       document.body.style.overflow = "hidden";
-//     }
-//     return () => {
-//       document.body.style.overflow = "auto";
-//     };
-//   }, [isOpen]);
-
-//   const modalContent = isOpen ? (
-//     <div className="position-fixed top-0 left-0 w-full h-full" ref={modalRef}>
-//       <div
-//         onClick={closeModal}
-//         className="bg-yellow-950 fixed inset-0 z-40 opacity-40"
-//       ></div>
-//       <div
-//         onClick={(e) => {
-//           e.stopPropagation();
-//           closeModal();
-//         }}
-//         className="fixed z-50 flex justify-center items-center inset-0 p-4"
-//       >
-//         <div
-//           style={{
-//             height: forceButtonFix ? "100%" : "unset",
-//           }}
-//           className="relative max-w-screen-sm w-full"
-//         >
-//           <div
-//             onClick={(e) => e.stopPropagation()}
-//             className="overflow-auto p-8 rounded-lg border-0 bg-red-700 w-full h-full"
-//           >
-//             <div className="w-full flex items-center justify-center">
-//               {/* <LoginOrRegister /> */}
-//               {children}
-//             </div>
-//           </div>
-//           <div className="absolute top-0 right-0 mt-2 mr-2 z-50">
-//             <Button onClick={closeModal} color="red">
-//               <CancelIcon width="24" height="24" />
-//             </Button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   ) : null;
-
-//   return modalContent;
-// };
-
-// export default ModalBase;
